9614ae9921dd2987faad24ce87f1a96e5c48cec2
pindhe <kharash420@gmail.com>
2026-06-27 16:08:37 +0300

    update

--- apps/companies/views.py
+++ apps/companies/views.py
@@ -1,5 +1,8 @@
+from django.db.models import Count, Q
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
+
+from apps.jobs.models import Job
 
 from .models import Company
 
@@ -16,6 +19,13 @@ class CompanyListView(ListView):
         if q:
             qs = qs.filter(name__icontains=q)
         return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["total_companies"] = Company.objects.count()
+        ctx["verified_companies"] = Company.objects.filter(is_verified=True).count()
+        ctx["open_jobs"] = Job.objects.filter(status=Job.Status.PUBLISHED).count()
+        return ctx
 
 
 class CompanyDetailView(DetailView):
